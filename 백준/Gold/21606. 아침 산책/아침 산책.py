56b077e14edf7b1a93462628ae0d839ffc04655d
류병현 <44202628+rbase0218@users.noreply.github.com>
2024-09-25 22:29:56 +0900

    [Gold III] Title: 아침 산책, Time: 696 ms, Memory: 68784 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -4,37 +4,52 @@ sys.setrecursionlimit(10**6)
 
 def bfs() -> int:
     q = deque()
-    answer = 0
+    indoor = 0
+    sum = 0
+    visit = [False] * (n_count + 1)
     
+    # 전체 순회
     for i in range(1, n_count + 1):
-        visit = [False] * (n_count + 1)
-        
-        if n_type[i - 1] == '0':
+        # 방문 여부 초기화 -> 새로운 노드가 돈다면
+        # 실내일 경우 제외한다.
+        if n_type[i - 1] == '1' or visit[i]:
             continue
-
-        visit[i] = True
+        
         q.append(i)
         
         while q:
-            curr_node = q.popleft()
-            visit[curr_node] = True
-            for adj_node in graph[curr_node]:
-                if visit[adj_node] == False:
-                    if n_type[adj_node - 1] == '1':
-                        answer += 1
-                    else:
-                        q.append(adj_node)
-    return answer
+            cur_node = q.popleft()
+            if n_type[cur_node - 1] == '0':
+                visit[cur_node] = True
+                
+            # 인접 노드 확인
+            for adj in graph[cur_node]:
+                if visit[adj]:
+                    continue
+                
+                if n_type[adj - 1] == '1':
+                    sum += 1
+                elif n_type[adj - 1] == '0':
+                    q.append(adj)
 
+        if sum <= 1:
+            sum = 0
+        indoor += sum * (sum - 1)
+        sum = 0
+    return indoor
+                
 if __name__ == '__main__':
+    answer = 0
     n_count = int(input())
     n_type = input()
 
-    graph = [[] for _ in range(n_count+1)]
+    graph = [ [] for _ in range(n_count+1)]
 
     for _ in range(n_count - 1):
         a, b = map(int, sys.stdin.readline().split())
         graph[a].append(b)
         graph[b].append(a)
-    
-    print(bfs())
+        
+        if n_type[a - 1] == n_type[b - 1] == '1':
+            answer += 2
+    print(bfs() + answer)
